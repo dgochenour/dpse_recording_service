78737b732a07d9aa3ce3b6d8de6b4e5c535388cd
Don Gochenour <don@rti.com>
2021-03-30 13:36:29 -0400

    configuration added for replay

--- discovery_constants.h
+++ discovery_constants.h
@@ -13,24 +13,25 @@
 #ifndef DISCOVERY_CONSTANTS_H
 #define DISCOVERY_CONSTANTS_H
 
-// constants for location_publisher_1
 static const char *k_PARTICIPANT01_NAME             = "location_publisher_1";
 static const int k_OBJ_ID_PARTICIPANT01_DW01        = 100;
 
-// constants for location_publisher_2 
 static const char *k_PARTICIPANT02_NAME             = "location_publisher_2";
 static const int k_OBJ_ID_PARTICIPANT02_DW01        = 110;
 
-// constants for location_subscriber
 static const char *k_PARTICIPANT03_NAME             = "location_subscriber";
 static const int k_OBJ_ID_PARTICIPANT03_DR01        = 120;
-
-// consts for rti_recording_service
-static const char *k_PARTICIPANT_RECORDING_NAME     = "rti_recording_service";
-static const int k_OBJ_ID_RECORDING_DR01            = 300;
 
 // contants for Admin Console
 static const char *k_PARTICIPANT_ADMINCONSOLE_NAME  = "Data Visualization";
 static const int k_OBJ_ID_ADMINCONSOLE_DR01         = 200;
+
+// constants for rti_recording_service
+static const char *k_PARTICIPANT_RECORDING_NAME     = "rti_recording_service";
+static const int k_OBJ_ID_RECORDING_DR01            = 300;
+
+// constants for rti_replay_service
+static const char *k_PARTICIPANT_REPLAY_NAME        = "rti_replay_service";
+static const int k_OBJ_ID_REPLAY_DW01               = 310;
 
 #endif
